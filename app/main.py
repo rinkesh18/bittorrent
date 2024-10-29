d296da97c4f0c27278f458676e05ed8433401dfd
rinkesh18 <rinkeshk700@gmail.com>
2024-10-29 10:55:25 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -24,6 +24,13 @@ def main():
                 return data.decode()
             raise TypeError(f"Type not serializable: {type(data)}")
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+    elif command == "info":
+        file_name = sys.argv[2]
+        with open(file_name, "rb") as torrent_file:
+            bencoded_content = torrent_file.read()
+        torrent = decode_bencode(bencoded_content)
+        print("Tracker URL:", torrent["announce"].decode())
+        print("Length:", torrent["info"]["length"])
     else:
         raise NotImplementedError(f"Unknown command {command}")
 if __name__ == "__main__":
