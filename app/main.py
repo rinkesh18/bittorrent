295b24c347bd9ee0963f2a4c51d7d44e2592ae0b
rinkesh18 <rinkeshk700@gmail.com>
2024-10-29 13:23:42 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -304,25 +304,20 @@ def receive_data(s):
 
 def main():
     command = sys.argv[1]
-    if command == 'magnet_handshake':
-        magnet_link = sys.argv[2]
-        magnet_handshake(magnet_link)
 
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
+    if command == "decode":
+        bencoded_value = sys.argv[2].encode()  # Ensure this is defined here
+        decoded_value, _ = decode_bencode(bencoded_value)
+
         def bytes_to_str(data):
             if isinstance(data, bytes):
                 return data.decode()
             raise TypeError(f"Type not serializable: {type(data)}")
 
-        decoded_value, _ = decode_bencode(bencoded_value)
         print(json.dumps(decoded_value, default=bytes_to_str))
 
     elif command == 'info':
         bencoded_file = sys.argv[2]
-
         decoded_value = get_decoded_value(bencoded_file)
         url = announce_url(decoded_value)
         info_dict = get_info_dict(decoded_value)
@@ -341,7 +336,6 @@ def main():
 
     elif command == 'peers':
         bencoded_file = sys.argv[2]
-
         ip_address_list = get_peer_address_torrent(bencoded_file)
         for ip_address in ip_address_list:
             print(ip_address)
@@ -419,147 +413,7 @@ def main():
         torrent_file = sys.argv[4]
 
         decoded_value = get_decoded_value(torrent_file)
-        url = announce_url(decoded_value)
-        info_dict = get_info_dict(decoded_value)
-        sha_info_hash = get_sha_info(info_dict)
-
-        ip_addresses = get_peer_address_torrent(torrent_file)
-        peer_ip, peer_port = ip_addresses[0].split(':')
-        peer_port = int(peer_port)
-
-        peer_id = '3a5f9c1e2d4a8e3b0f6c'
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        response_peer_id = ping_peer_torrent(peer_ip, peer_port, sha_info_hash, peer_id, s)
-
-        total_length = info_dict['length']
-        piece_length = info_dict['piece length']
-
-        # Bitfield
-        s.recv(4)
-        s.recv(1)
-        s.recv(4)
-
-        # Interested
-        s.sendall(b'\x00\x00\x00\x01')
-        s.sendall(b'\x02')
-        # Unchoke
-        s.recv(4)
-        s.recv(1)
-
-        for i in range(0, total_length, piece_length):
-            curr_piece_length = min(piece_length, total_length - i)
-            block_size = 2 ** 14
-            curr_sent_data_size = 0
-            iterations = 0
-
-            while curr_sent_data_size < curr_piece_length:
-                data_size_to_send = min(block_size, curr_piece_length - curr_sent_data_size)
-                curr_sent_data_size += data_size_to_send
-                send_data(s, i // piece_length, iterations * block_size, data_size_to_send)
-                iterations += 1
-
-            result_data = b''
-            for i in range(0, iterations):
-                result_data += receive_data(s)
-
-            with open(download_location, "ab") as f:
-                f.write(result_data)
-
-    elif command == 'magnet_parse':
-        magnet_link = sys.argv[2]
-        info_hash_location = magnet_link.find('btih:') + 5
-        info_hash = magnet_link[info_hash_location:info_hash_location + 40]
-        url_location = magnet_link.find('tr=') + 3
-        url = magnet_link[url_location:]
-        print(f'Tracker URL: {urllib.parse.unquote(url)}')
-        print(f'Info Hash: {info_hash}')
-
-    elif command == 'magnet_handshake':
-        magnet_link = sys.argv[2]
-        info_hash_location = magnet_link.find('btih:') + 5
-        info_hash = magnet_link[info_hash_location:info_hash_location + 40]
-        url_location = magnet_link.find('tr=') + 3
-        url = magnet_link[url_location:]
-        url = urllib.parse.unquote(url)
-        ip_addresses = get_peer_address_magnet(url, info_hash)
-        peer_ip, peer_port = ip_addresses[0].split(':')
-        peer_port = int(peer_port)
-
-        peer_id = '3a5f9c1e2d4a8e3b0f6c'
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        response_peer_id = ping_peer_magnet(peer_ip, peer_port, info_hash, peer_id, s)
-        print(f'Peer ID: {response_peer_id}')
-
-        # Bitfield
-        s.recv(4)
-        s.recv(1)
-        s.recv(4)
-
-        magnet_dict = {"m": {
-            "ut_metadata": 18
-        }}
-
-        encoded_magnet_dict = bencodepy.encode(magnet_dict)
-        s.sendall(integer_to_byte(len(encoded_magnet_dict) + 2))
-        s.sendall(b'\x14')
-        s.sendall(b'\x00')
-        s.sendall(encoded_magnet_dict)
-
-        payload_size = byte_to_integer(s.recv(4)) - 2
-        s.recv(1)
-        s.recv(1)
-        handshake_message = s.recv(payload_size)
-        handshake_message = decode_bencode(handshake_message)
-        # print(f'Peer Metadata Extension ID: {handshake_message[0]['m']['ut_metadata']}')
-        print(f'Peer Metadata Extension ID: {handshake_message[0]["m"]["ut_metadata"]}')
-
-    elif command == 'magnet_info':
-        magnet_link = sys.argv[2]
-        info_hash_location = magnet_link.find('btih:') + 5
-        info_hash = magnet_link[info_hash_location:info_hash_location + 40]
-        url_location = magnet_link.find('tr=') + 3
-        url = magnet_link[url_location:]
-        url = urllib.parse.unquote(url)
-        ip_addresses = get_peer_address_magnet(url, info_hash)
-        peer_ip, peer_port = ip_addresses[0].split(':')
-        peer_port = int(peer_port)
-
-        peer_id = '3a5f9c1e2d4a8e3b0f6c'
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        response_peer_id = ping_peer_magnet(peer_ip, peer_port, info_hash, peer_id, s)
-
-        s.recv(4)
-        s.recv(1)
-        s.recv(4)
-
-        magnet_dict = {"m": {
-            "ut_metadata": 18
-        }}
-
-        encoded_magnet_dict = bencodepy.encode(magnet_dict)
-        s.sendall(integer_to_byte(len(encoded_magnet_dict) + 2))
-        s.sendall(b'\x14')
-        s.sendall(b'\x00')
-        s.sendall(encoded_magnet_dict)
-
-        payload_size = byte_to_integer(s.recv(4)) - 2
-        s.recv(1)
-        s.recv(1)
-        handshake_message = s.recv(payload_size)
-
-        request_metadata = {
-            'msg_type': 0,
-            'piece': 0
-        }
-
-        request_metadata = bencodepy.encode(request_metadata)
-        s.sendall(integer_to_byte(len(request_metadata) + 2))
-        s.sendall(b'\x14')
-        s.sendall(b'\x00')
-        s.sendall(request_metadata)
-
-    else:
-        raise NotImplementedError(f"Unknown command {command}")
+        url = announce_url
 
 
 if __name__ == "__main__":
